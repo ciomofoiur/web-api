0a872a8c7e71f1f1bd9b6d7ce5fed938ac6a7dd3
Ciomy <ciomofoiuraoul@gmail.com>
2023-08-22 13:20:21 +0300

    final

---
+++ BookAPI/Controllers/BookController.cs
@@ -0,0 +1,59 @@
+﻿using BookAPI.Services.BookService;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Xml.Linq;
+
+namespace BookAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+        private readonly IBookService _bookService;
+
+        public BookController(IBookService bookService)
+        {
+            _bookService = bookService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Book>>> GetAllBooks()
+        {
+            return await _bookService.GetAllBooks();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Book>> GetSingleBook(int id)
+        {
+            var result = await _bookService.GetSingleBook(id);
+            if (result is null)
+                return NotFound("Book not found :(");
+            return Ok(result);
+        }
+
+        [HttpPost("")]
+        public async Task<ActionResult<Book>> AddBook(Book book)
+        {
+            var result = await _bookService.AddBook(book);
+            return Created("~/api/book/" + book.Id, result);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<List<Book>>> UpdateBook(int id, Book requestedBook)
+        {
+            var result = await _bookService.UpdateBook(id, requestedBook);
+            if (result is null)
+                return NotFound("Book not found :(");
+            return Ok(result);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<List<Book>>> DeleteBook(int id)
+        {
+            var result = await _bookService.DeleteBook(id);
+            if (result is null)
+                return NotFound("Book not found :(");
+            return NoContent();
+        }
+    }
+}
---
+++ BookAPI/Services/BookService/IBookService.cs
@@ -0,0 +1,11 @@
+﻿namespace BookAPI.Services.BookService
+{
+    public interface IBookService
+    {
+        Task<List<Book>> GetAllBooks();
+        Task<Book?> GetSingleBook(int id);
+        Task<List<Book>> AddBook(Book book);
+        Task<List<Book>?> UpdateBook(int id, Book requestedBook);
+        Task<List<Book>?> DeleteBook(int id);
+    }
+}
