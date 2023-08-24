7b8c0be0f031f37e7e56fa5a1ccce13beb12dff7
Ciomy <ciomofoiuraoul@gmail.com>
2023-08-24 18:02:17 +0300

    added validation attributes

--- BookAPI/Models/Book.cs
+++ BookAPI/Models/Book.cs
@@ -1,11 +1,22 @@
-﻿namespace BookAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookAPI.Models
 {
     public class Book
     {
         public int Id { get; set; }
-        public string? Name { get; set; }
+
+        [Required]
+        public string Name { get; set; } = string.Empty;
+
+        [Range(0, 999.99, ErrorMessage = "Price can't exceed 1000")]
         public decimal Price { get; set; }
+
+        [StringLength(10)]
         public string? Category { get; set; }
+
+        [RegularExpression(@"^[a-zA-Z\s]{1,40}$",
+         ErrorMessage = "Characters are not allowed.")]
         public string? Author { get; set; }
     }
 }
